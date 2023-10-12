76be5cb6985770a971d5ebafee18f6c337f35731
Palak <palak.desai4501@gmail.com>
2023-10-12 22:35:28 +0530

    Feature: Changed Size Of Icons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,8 +10,8 @@ const SideBar = () => {
           <img
             src="src/assets/Home.png"
             alt="feed"
-            height="50px"
-            width="50px"
+            height="40px"
+            width="40px"
           />
           <span>Feed</span>
         </button>
@@ -20,8 +20,8 @@ const SideBar = () => {
           <img
             src="src/assets/Explore.png"
             alt="Explore"
-            height="50px"
-            width="50px"
+            height="40px"
+            width="40px"
           />
           <span>Explore</span>
         </button>
@@ -30,8 +30,8 @@ const SideBar = () => {
           <img
             src="src/assets/Save.png"
             alt="Save"
-            height="50px"
-            width="50px"
+            height="40px"
+            width="40px"
           />
           <span>Save</span>
         </button>
@@ -40,8 +40,8 @@ const SideBar = () => {
           <img
             src="src/assets/Messages.png"
             alt="Messages"
-            height="50px"
-            width="50px"
+            height="40px"
+            width="40px"
           />
           <span>Messages</span>
         </button>
@@ -50,8 +50,8 @@ const SideBar = () => {
           <img
             src="src/assets/People.png"
             alt="People"
-            height="50px"
-            width="50px"
+            height="40px"
+            width="40px"
           />
           <span>People</span>
         </button>
