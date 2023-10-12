72633ee30f7f8c3e604fa036cb98f19172ff4e33
Palak <palak.desai4501@gmail.com>
2023-10-12 13:31:08 +0530

    Feature: Added Profile Component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Profile from './Profile'
 
 const SideBar = () => {
   return (
-    <div>
-      SideBar
+    <div className='sidebar'>
+      <Profile />
     </div>
   )
 }
