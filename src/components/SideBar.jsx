0a36a6bf81b16ab738d5785f775a4ecbf4cf53ea
Palak <palak.desai4501@gmail.com>
2023-10-12 18:11:22 +0530

    Feature: Added Styles

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,6 +5,23 @@ const SideBar = () => {
   return (
     <div className='sidebar'>
       <Profile />
+      <div className='container'>
+        <div className='logo'>
+            <img src="src/assets/Home.png" alt="feed" />
+        </div>
+        <div className='logo'>
+            <img src="src/assets\Explore.png" alt="Explore" />
+        </div>
+        <div className='logo'>
+            <img src="src/assets\Save.png" alt="Save" />
+        </div>
+        <div className='logo'>
+            <img src="src/assets\Messages.png" alt="Messages" />
+        </div>
+        <div className='logo'>
+            <img src="src/assets\People.png" alt="People" />
+        </div>
+      </div>
     </div>
   )
 }
