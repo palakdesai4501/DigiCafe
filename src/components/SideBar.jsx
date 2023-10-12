7baba9b910a2fae5475764d487fdf423eaff955a
Palak <palak.desai4501@gmail.com>
2023-10-12 18:50:56 +0530

    Feature: Added buttons and Icons and applied hover effect

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import Profile from './Profile'
+import React from "react";
+import Profile from "./Profile";
 
 const SideBar = () => {
-  return (
-    <div className='sidebar'>
-      <Profile />
-      <div className='container'>
-        <div className='logo'>
-            <img src="src/assets/Home.png" alt="feed" />
-        </div>
-        <div className='logo'>
-            <img src="src/assets\Explore.png" alt="Explore" />
-        </div>
-        <div className='logo'>
-            <img src="src/assets\Save.png" alt="Save" />
-        </div>
-        <div className='logo'>
-            <img src="src/assets\Messages.png" alt="Messages" />
-        </div>
-        <div className='logo'>
-            <img src="src/assets\People.png" alt="People" />
-        </div>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="sidebar">
+            <Profile />
+            <div className="container">
+                <button className="category-btn">
+                    <img src="src/assets/Home.png" alt="feed" height='50px' width='50px' />
+                    <span>Feed</span>
+                </button>
 
-export default SideBar
+                <button className="category-btn">
+                    <img src="src/assets/Explore.png" alt="Explore" height='50px' width='50px' />
+                    <span>Explore</span>
+                </button>
+
+                <button className="category-btn">
+                    <img src="src/assets/Save.png" alt="Save" height='50px' width='50px' />
+                    <span>Save</span>
+                </button>
+
+                <button className="category-btn">
+                    <img src="src/assets/Messages.png" alt="Messages" height='50px' width='50px' />
+                    <span>Messages</span>
+                </button>
+
+                <button className="category-btn">
+                    <img src="src/assets/People.png" alt="People" height='50px' width='50px' />
+                    <span>People</span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;
