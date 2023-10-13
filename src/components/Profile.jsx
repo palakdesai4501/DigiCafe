808e7f4099f4d91bdcd1245f16de780b9264e2bc
Palak <palak.desai4501@gmail.com>
2023-10-13 21:45:50 +0530

    Feature: Styled Profile Container

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Profile = () => {
   return (
     <div className='profile'>
-      Profile
+      <div className="profile-container">
+      <img src="/src/assets/profile.png" alt="Profile" className="profile-image" />
+      <div className="profile-name">Palak Desai</div>
+    </div>
+      
     </div>
   )
 }
