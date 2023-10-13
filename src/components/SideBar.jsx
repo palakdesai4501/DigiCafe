480b65803eb3e78572c9b09e38cf993615e808e2
Palak <palak.desai4501@gmail.com>
2023-10-13 11:16:41 +0530

    Feature: Added Sidebar Icons via map function

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,60 +1,18 @@
 import React from "react";
 import Profile from "./Profile";
+import { items } from "../utils/constants";
 
 const SideBar = () => {
   return (
     <div className="sidebar">
       <Profile />
       <div className="container">
-        <button className="category-btn">
-          <img
-            src="src/assets/Home.png"
-            alt="feed"
-            height="40px"
-            width="40px"
-          />
-          <span>Feed</span>
-        </button>
-
-        <button className="category-btn">
-          <img
-            src="src/assets/Explore.png"
-            alt="Explore"
-            height="40px"
-            width="40px"
-          />
-          <span>Explore</span>
-        </button>
-
-        <button className="category-btn">
-          <img
-            src="src/assets/Save.png"
-            alt="Save"
-            height="40px"
-            width="40px"
-          />
-          <span>Save</span>
-        </button>
-
-        <button className="category-btn">
-          <img
-            src="src/assets/Messages.png"
-            alt="Messages"
-            height="40px"
-            width="40px"
-          />
-          <span>Messages</span>
-        </button>
-
-        <button className="category-btn">
-          <img
-            src="src/assets/People.png"
-            alt="People"
-            height="40px"
-            width="40px"
-          />
-          <span>People</span>
-        </button>
+        {items.map((item, index) => (
+          <button key={index} className="category-btn">
+            {item.icon}
+            <span>{item.name}</span>
+          </button>
+        ))}
       </div>
     </div>
   );
