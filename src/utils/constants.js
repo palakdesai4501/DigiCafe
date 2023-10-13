e8c801dd0f65c96303e0fb8dc15790b97b19a3b8
Palak <palak.desai4501@gmail.com>
2023-10-13 10:53:48 +0530

    Feature: Added Icons

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+import AiFillHome from 'react-icons/ai';
+import  MdOutlineExplore from 'react-icons/md' ;
+import BsFillBookmarkFill from 'react-icons/bs';
+import AiFillMessage from 'react-icons/ai';
+import BsFillPeopleFill from 'react-icons/bs'
+
+export const icons = [
+    {name: 'Home', icon:<AiFillHome />, },
+    {name: 'Explore', icon:<MdOutlineExplore />, },
+    {name: 'Saved', icon:<BsFillBookmarkFill />, },
+    {name: 'Messages', icon:<AiFillMessage />, },
+    {name: 'People', icon:<BsFillPeopleFill />, },
+
+]
