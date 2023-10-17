803fffcd8badbc2bf8957ccc833bf6149cd123a8
Palak <palak.desai4501@gmail.com>
2023-10-17 17:41:25 +0530

    Feature: Give Style to search-bar

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Title = ({title}) => {
+const Title = ({ title }) => {
   return (
-    <div className='title-container'>
+    <div className="title-container">
       <h1>{title}</h1>
+      <div>
+        <input type="text" className="search-bar" placeholder="Search..." />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
