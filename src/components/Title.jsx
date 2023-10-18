6ee6b2dd0e505c60e1ed36c48519121c26d1c9bc
Palak <palak.desai4501@gmail.com>
2023-10-18 20:15:06 +0530

    Feature: Made changes to search-bar

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
 
 const Title = ({ title }) => {
   return (
     <div className="title-container">
       <h1>{title}</h1>
-      <div>
-        <input type="text" className="search-bar" placeholder="Search..." />
+      <div className="search-container">
+        <input type="text"  placeholder="Search..." />
+      
+          <BiSearchAlt2  className="search-icon"/>
+    
       </div>
     </div>
   );
