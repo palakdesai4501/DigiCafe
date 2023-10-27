3ab369f1897dc73a4169016be4a6e48d6acddc78
Palak <palak.desai4501@gmail.com>
2023-10-27 18:50:35 +0530

    Feature: Added icon

--- src/components/PostStoryProfile.jsx
+++ src/components/PostStoryProfile.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import {BsThreeDots} from 'react-icons/bs';
 
 const PostStoryProfile = ({ name }) => {
   return (
     <div className="post-profile">
       <div className="post-container"></div>
       <div className="profile-name">{name}</div>
+      <BsThreeDots className='dot-icon'/>
     </div>
   );
 };
