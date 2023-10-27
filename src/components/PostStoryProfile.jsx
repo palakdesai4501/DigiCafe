8d4aee1c154fa6f2d117ecc8a875d274b1b3b3ce
Palak <palak.desai4501@gmail.com>
2023-10-27 18:39:48 +0530

    Feature: Added profile name

--- src/components/PostStoryProfile.jsx
+++ src/components/PostStoryProfile.jsx
@@ -4,6 +4,7 @@ const PostStoryProfile = ({ name }) => {
   return (
     <div className="post-profile">
       <div className="post-container"></div>
+      <div className="profile-name">{name}</div>
     </div>
   );
 };
