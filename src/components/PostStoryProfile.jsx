07346c799e6b24a9c624ded245b500899114b553
Palak <palak.desai4501@gmail.com>
2023-10-28 15:24:59 +0530

    Feature: Formatted document and changed height of post-img

--- src/components/PostStoryProfile.jsx
+++ src/components/PostStoryProfile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import {BsThreeDots} from 'react-icons/bs';
+import { BsThreeDots } from "react-icons/bs";
 
 const PostStoryProfile = ({ name }) => {
   return (
     <div className="post-profile">
       <div className="post-container"></div>
       <div className="profile-name">{name}</div>
-      <BsThreeDots className='dot-icon'/>
+      <BsThreeDots className="dot-icon" />
       <div className="post">
-        <img src="/src/assets/pizza.jpg" alt='post-img' className="post-img"/>
+        <img src="/src/assets/pizza.jpg" alt="post-img" className="post-img" />
       </div>
     </div>
   );
